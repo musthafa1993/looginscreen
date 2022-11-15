ec6eda41b03385e6089f0a3b5a48a1797a0ac1f6
tanspmartin <tanspmartin@gmail.com>
2022-11-15 15:49:21 +0530

    sent

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -8,13 +8,18 @@ class Home extends StatefulWidget {
 }
 //home page
 class _HomeState extends State<Home> {
+  var names=["anu","achu","raju","peli","raman","anathu","parvathi"];
+  var mobile_number = ["9845345687","9543543454","90495859595","94485857474","94875938847","0498509584039","94049303940"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("contacts"),
+        actions: [Icon(Icons.search)],
+      ),
       body: SafeArea(
        child: ListView.builder(
-
-         itemCount: 25,
+         itemCount: names.length,
            itemBuilder: (context,int index){
          return Padding(
            padding: const EdgeInsets.all(8.0),
@@ -22,13 +27,16 @@ class _HomeState extends State<Home> {
              tileColor: Colors.green,
              leading: Icon(Icons.account_circle,
              color: Colors.white,),
-             title: Text("Name $index" ,
+             title: Text(
+             "${names[index]}",
+               
                style: TextStyle(
-
                  fontSize: 15,
                    fontWeight: FontWeight.w500,
                    color: Colors.white),),
-             trailing: Icon(Icons.phone,color: Colors.white,),
+             subtitle: Text("${mobile_number[index]}",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 14,color: Colors.white),),
+             trailing: Icon(
+               Icons.phone,color: Colors.white,),
            ),
          );
        }),
