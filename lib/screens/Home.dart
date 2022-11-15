5a0d6d7c40369dfaceb946b4d5cef827c76b5825
tanspmartin <tanspmartin@gmail.com>
2022-11-15 10:25:56 +0530

    edit

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -12,40 +12,33 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-              buildContainer(),
-            ],
-          ),
-        ),
+       child: ListView.builder(
+
+         itemCount: 25,
+           itemBuilder: (context,int index){
+         return Padding(
+           padding: const EdgeInsets.all(8.0),
+           child: ListTile(
+             tileColor: Colors.green,
+             leading: Icon(Icons.account_circle,
+             color: Colors.white,),
+             title: Text("Name $index" ,
+               style: TextStyle(
+
+                 fontSize: 15,
+                   fontWeight: FontWeight.w500,
+                   color: Colors.white),),
+             trailing: Icon(Icons.phone,color: Colors.white,),
+           ),
+         );
+       }),
       )
     );
   }
 
-  Container buildContainer() {
+  Container buildContainer(int index) {
     return Container(
+      child: Center(child: Text("$index"),),
             margin: EdgeInsets.all(8),
           height: 60,
             width: double.infinity,
